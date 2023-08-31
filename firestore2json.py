cb658a63ee1f5997c83963c430bbf2413dd6247a
jwc20 <choij2015@gmail.com>
2023-08-31 22:38:28 +0900

    Add firestore to json script

---
+++ firestore2json.py
@@ -0,0 +1,21 @@
+import json
+from google.cloud import firestore
+
+# Initialize Firestore client with credentials
+db = firestore.Client.from_service_account_json("credentials.json")
+
+# Choose your Firestore collection
+# collection_name = 'chat'
+# collection_ref = db.collection(collection_name)
+collection_ref = db.collection('chats').document('sodapoppin').collection('2023').document('08').collection('31')
+
+# Fetch documents from the collection
+docs = collection_ref.stream()
+
+# Convert documents into a list of dictionaries
+data = [doc.to_dict() for doc in docs]
+
+# Export data to JSON file
+with open(f'output.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4)
+

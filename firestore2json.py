daa4c58f4971fd3ef8f1d0fbe17769e8e302de1d
jwc20 <choij2015@gmail.com>
2023-09-03 12:39:42 +0900

    Refactor script for datetime usage and dynamic channel naming in filenames

--- firestore2json.py
+++ firestore2json.py
@@ -1,21 +1,17 @@
 import json
 from google.cloud import firestore
-import datetime
+from datetime import datetime
 
+
+channel_name = "sodapoppin"
 db = firestore.Client.from_service_account_json("credentials.json")
-collection_ref = db.collection('chats').document('sodapoppin').collection('2023').document('09').collection('02')
+collection_ref = db.collection('chats').document(channel_name).collection('2023').document('09').collection('02')
 docs = collection_ref.stream()
 data = [doc.to_dict() for doc in docs]
 
-filename = 'chat_' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M") + '.json'
+filename = channel_name + '_chat_' + datetime.now().strftime("%Y-%m-%d_%H-%M") + '.json'
 with open(filename, 'w') as outfile:
     length = len(data)
     print("Number of hours recorded: ",length)
-    json.dump(data, outfile)
-    
-    
-
-
-
-
+    json.dump(data[0]['chats'], outfile, indent=4)
 
